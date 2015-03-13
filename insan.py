0af0342bca5f9db9abad8c0db99624d2f3c6f4cb
Ilker Manap <ilkermanap@gmail.com>
2015-03-13 22:11:32 +0100

    insan nesnesi rapor eklendi

--- insan.py
+++ insan.py
@@ -6,9 +6,25 @@ class Insan:
         self.cins = cinsiyeti
         self.dogumyili = dogumyili
 
+    def rapor(self):
+        print "adi        :", self.adi
+        print "dogum yili :", self.dogumyili 
+
 class Calisan(Insan):
     def __init__(self, ad, cins, dy, maas):
         Insan.__init__(self, ad, cins, dy)
         self.maas = maas
 
-    
+    def rapor(self):
+        Insan.rapor(self)
+        print "maasi :", self.maas
+
+
+a = Insan("ilker","e",1972)
+
+b = Calisan("ilker","e",1972, 100)
+
+a.rapor()
+b.rapor()
+
+
