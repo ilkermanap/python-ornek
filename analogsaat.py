40a682593cf30f04729cc93e76d245f8dbfd4918
ilkermanap <ilkermanap@gmail.com>
2015-09-21 22:28:12 +0200

    analog saat kodu

--- analogsaat.py
+++ analogsaat.py
@@ -14,23 +14,21 @@ def analog_saat(r=100):
     m = t.tm_min
     s = t.tm_sec
 
-    print h, m, s
     secangle = radian(s * 6)
     sx = r * math.sin(secangle)
     sy = r * math.cos(secangle)
 
-    print sx, sy
-
     minangle = radian(m * 6)
     mx = r * math.sin(minangle)
     my = r * math.cos(minangle)
-    print mx, my
 
     hangle = radian((h * 30) + ((m / 60) * 30))
     hx = r * math.sin(hangle)
     hy = r * math.cos(hangle)
-    print hx, hy
     return ((hx, hy), (mx, my), (sx, sy))
 
 
-analog_saat()
+while 1:
+    h, m, s = analog_saat()
+    print h[0], h[1], m[0], m[1], s[0], s[1]
+    time.sleep(1)
